624af04f3730b9eff696138302e40dd1bba49d5b
ReshiAdavan <reshiadavan27@gmail.com>
2022-11-01 14:17:53 -0400

    Maximum SubArray - Kadanes Algorithm: Simply compare the localMax computed for a partition of an array with that localMax and then next element. Perform this n times and store the globalMax as the max of the localMax. Dynamic programming approach

---
+++ MaximumSubArray.py
@@ -0,0 +1,21 @@
+# Kadane’s Algorithm - DP
+# https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        length = len(nums)
+        if length == 1:
+            return nums[0]
+        localMax = 0
+        globalMax = float('-inf')
+
+        x = 0
+        while x < length:
+            localMax = max(nums[x], nums[x] + localMax)
+            if localMax > globalMax:
+                globalMax = localMax
+            x += 1
+        return globalMax
+
+# Beats 91.71% python submissions in runtime
+# Beats 72.13% python submissions in memory usage
