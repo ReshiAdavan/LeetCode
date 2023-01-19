81b93e858a9fda16300be546940c2ed23c2d7efc
ReshiAdavan <reshiadavan27@gmail.com>
2023-01-19 11:34:13 -0500

    Check String Contains All Binary Codes of Size K - Find every substring of size k and store it into a set. Compare if the size of the set equals the total number of possible substrings of size k. If the set does not contain every possible substring, then return false.

--- CheckStringContainsAllBinaryCodesOfSizeK.py
+++ CheckStringContainsAllBinaryCodesOfSizeK.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def hasAllCodes(self, s, k):
-        pass
+        codeSet = set()
+        self.subString(s, k, codeSet)
+        return len(codeSet) == 2 ** k
 
-# Beats % python submissions in runtime
-# Beats % python submissions in memory usage
+    def subString(self, s, k, codeSet):
+        left, right = 0, k - 1
+
+        while right < len(s):
+            if s[left:right + 1] not in codeSet:
+                codeSet.add(s[left:right + 1])
+
+            left += 1
+            right += 1
+
+# Beats 70.45% python submissions in runtime
+# Beats 70.45% python submissions in memory usage
