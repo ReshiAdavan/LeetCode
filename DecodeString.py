cde9ac8067fa0ca99ebbdd98ce9baace69ad0858
ReshiAdavan <reshiadavan27@gmail.com>
2023-01-24 12:09:43 -0500

    Decode Strings - Recursion or using a stack works; I chose to use a stack. Solution: Append every letter in s into the stack until you encounter a closing bracket. Once you encounter a closing bracket, create a substr that pre-pends (antonym for appends) the popped letter from the stack. Now if you encounter a digit, say k, multiply the substr k times and append it to the stack. Repeat this process as all the brackets and integers will be replaced with their respective decodings and a final string of only letters would be present in the stack. Pop all letters from the stack and then return that as a string.

---
+++ DecodeString.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s):
+        stack, length, result = [], len(s), ""
+
+        for i in range(length): 
+            if s[i] != ']': stack.append(s[i])
+            else: 
+                substr = ""
+                while stack[-1] != "[": substr = stack.pop() + substr
+                stack.pop()
+
+                k = ""
+                while stack and stack[-1].isdigit(): k = stack.pop() + k
+                stack.append(int(k) * substr)
+
+        result += ''.join(stack)
+        return result
+
+# Beats 71.33% python submissions in runtime
+# Beats 82.47% python submissions in memory usage
