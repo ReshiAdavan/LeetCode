d9274a74cea78fb74908bcf859c76bae8af0a471
ReshiAdavan <reshiadavan27@gmail.com>
2023-02-22 16:51:40 -0500

    Missed deletes and also moidifed README.md

--- LengthOfLastWord.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        res = s.split()
-        return len(res[-1])
-
-# Beats 89.14% python submissions in runtime
-# Beats 74.57% python submissions in memory usage
