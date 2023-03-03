d8b33e5d11a57ae1172a485e67b81404e108ea2f
ReshiAdavan <reshiadavan27@gmail.com>
2023-03-03 18:39:15 -0500

    Moving dir into Python

--- Arrays-Maps/GroupAnagrams.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        H = defaultdict(list)
-        for s in strs:
-            H[str(sorted(s))].append(s)
-        return H.values()
-
-# Beats 47.82% python submissions in runtime
-# Beats 46.49% python submissions in memory usage  
