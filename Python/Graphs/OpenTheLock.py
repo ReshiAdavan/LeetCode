f4caa92806020852000d00b3c5c9e60db31cc74c
ReshiAdavan <reshiadavan27@gmail.com>
2023-09-22 13:04:57 -0400

    Open the Lock: BFS and we generate new lock patterns with str int manipulation and a for loop. Pretty conventional <graph> problem

---
+++ Python/Graphs/OpenTheLock.py
@@ -0,0 +1,35 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+
+        def generatePatterns(s: str):
+            res = []
+
+            for i in range(4):
+                # digit = digit + 1 (0 -> 1)
+                digit = str((int(s[i]) + 1) % 10)
+                res.append(s[:i] + digit + lock[i+1:])
+
+                # digit = digit - 1 (9 -> 8)
+                digit = str((int(s[i]) - 1 + 10) % 10)
+                res.append(s[:i] + digit + lock[i+1:])
+            return res
+
+        q = deque()
+        q.append(["0000", 0]) # [string, # of moves/turns]
+        visited = set(deadends)
+
+        while q:
+            lock, turns = q.popleft()
+
+            if lock == target: 
+                return turns
+            for pattern in generatePatterns(lock):
+                if pattern not in visited:
+                    visited.add(pattern)
+                    q.append([pattern, turns + 1]) # [string, # of moves/turns]
+        return -1
+    
+# Beats 57.76% python submissions in runtime
+# Beats 92.19% python submissions in memory usage
