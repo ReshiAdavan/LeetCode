3f3ca5452f1ad2ed76eaceb391e4fb705bf4f059
ReshiAdavan <reshiadavan27@gmail.com>
2023-09-21 22:15:38 -0400

    Shortest Path With Alternating Colors: BFS where we check if we visit a node, then after update the answer list with a length if unupdated. Keep track of nodes twice, once for red and blue, only add to queue if alternating color condition has been met.

---
+++ Python/Graphs/ShortestPathWithAlternatingColors.py
@@ -0,0 +1,34 @@
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        redMap = collections.defaultdict(list)
+        blueMap = collections.defaultdict(list)
+
+        for src, dest in redEdges: redMap[src].append(dest)
+        for src, dest in blueEdges: blueMap[src].append(dest)
+
+        answer = [-1 for i in range(n)]
+        q = deque()
+        q.append([0, 0, None]) # node, len, color
+        visited = set()
+        visited.add((0, None)) # node, color
+
+        while q:
+            node, length, edgeColor = q.popleft()
+            if answer[node] == -1: 
+                answer[node] = length
+            
+            if edgeColor != "RED":
+                for nei in redMap[node]:
+                    if (nei, "RED") not in visited:
+                        visited.add((nei, "RED"))
+                        q.append([nei, length + 1, "RED"])
+            
+            if edgeColor != "BLUE":
+                for nei in blueMap[node]:
+                    if (nei, "BLUE") not in visited:
+                        visited.add((nei, "BLUE"))
+                        q.append([nei, length + 1, "BLUE"])
+        return answer
+    
+# Beats 94.44% python submissions in runtime
+# Beats 89.44% python submissions in memory usage 
