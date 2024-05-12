2cad6f59b28f9e62c9770535d0ed1550fbd7feb4
ReshiAdavan <reshiadavan27@gmail.com>
2024-05-12 09:32:07 -0400

    LC-06: Backtracking

--- Python/Backtracking/Permutations.py
+++ Python/Backtracking/Permutations.py
@@ -17,3 +17,23 @@ class Solution(object):
 
 # Beats 93.59% python submissions in runtime
 # Beats 16.29% python submissions in memory usage
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(step):
+            if step == len(nums):
+                res.append(nums.copy())
+                return
+
+            for i in range(step, len(nums)):
+                nums[step], nums[i] = nums[i], nums[step]
+                dfs(step + 1)
+                nums[step], nums[i] = nums[i], nums[step]
+            
+        dfs(0)
+        return res
+
+# Beats 97.94% python submissions in runtime
+# Beats 77.54% python submissions in memory usage
--- Python/Backtracking/Subsets.py
+++ Python/Backtracking/Subsets.py
@@ -17,3 +17,25 @@ class Solution(object):
 
 # Beats 87.58% python submissions in runtime
 # Beats 70.43% python submissions in memory usage
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [] 
+        subset = []
+
+        def dfs(step):
+            if step >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[step])
+            dfs(step + 1)
+
+            subset.pop()
+            dfs(step + 1)
+
+        dfs(0)
+        return res
+
+# Beats 90.55% python submissions in runtime
+# Beats 77.72% python submissions in memory usage
