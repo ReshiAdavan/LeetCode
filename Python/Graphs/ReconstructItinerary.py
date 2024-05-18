cb73d0d758cb66388b232a16b651ae3f860547f1
ReshiAdavan <reshiadavan27@gmail.com>
2024-05-18 17:27:06 -0400

    LC-11

---
+++ Python/Graphs/ReconstructItinerary.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = {src: [] for src, dst in tickets}
+        res = []
+
+        for src, dst in tickets:
+            graph[src].append(dst)
+
+        for key in graph:
+            graph[key].sort()
+
+        def dfs(graph, result, src):
+            if src in graph:
+                destinations = graph[src][:]
+                while destinations:
+                    dest = destinations[0]
+                    graph[src].pop(0)
+                    dfs(graph, result, dest)
+                    destinations = graph[src][:]
+            result.append(src)
+
+        dfs(graph, res, "JFK")
+        res.reverse()
+
+        if len(res) != len(tickets) + 1:
+            return []
+
+        return res
+    
+# Beats 96.50% of users with Python3 in runtime
+# Beats 34.43% of users with Python3 in memory
