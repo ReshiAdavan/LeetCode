208f9d3c79b451a5acd819f647461f65b5c67142
ReshiAdavan <reshiadavan27@gmail.com>
2024-05-25 18:14:34 -0400

    LC-15: Graphs + BT practice

--- Python/Backtracking/NQueens.py
+++ Python/Backtracking/NQueens.py
@@ -30,3 +30,34 @@ class Solution(object):
 
 # Beats 69.48% python submissions in runtime
 # Beats 60.30% python submissions in memory usage 
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        cols, pDiag, nDiag = set(), set(), set()
+        board = [["."] * n for _ in range(n)]
+
+        def dfs(i):
+            if i >= n:
+                soln = ["".join(row) for row in board] 
+                res.append(soln)
+                return
+            
+            for k in range(n):
+                if k not in cols and (k - i) not in nDiag and (k + i) not in pDiag:
+                    cols.add(k)
+                    pDiag.add(k + i)
+                    nDiag.add(k - i)
+                    board[i][k] = "Q"
+                    dfs(i + 1)
+                    board[i][k] = "."
+                    cols.remove(k)
+                    pDiag.remove(k + i)
+                    nDiag.remove(k - i)
+            return
+
+        dfs(0)
+        return res
+    
+# Beats 83.75% of users with Python3 in runtime 
+# Beats 79.78% of users with Python3 in memory
