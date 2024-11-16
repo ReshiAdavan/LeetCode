d7bdabf4d7681d5f5bdd507f4a3392e29d9e558b
ReshiAdavan <reshiadavan27@gmail.com>
2024-11-16 18:08:58 -0500

    LC-45: Trees

--- DSA/Trees/DiameterOfABinaryTree.py
+++ DSA/Trees/DiameterOfABinaryTree.py
@@ -1,9 +1,10 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root):
         self.d = 0
@@ -18,4 +19,29 @@ class Solution:
         return max(left, right) + 1
 
 # Beats 67.65%  python submissions in runtime
-# Beats 98.15% python submissions in memory usage  
+# Beats 98.15% python submissions in memory usage
+
+from typing import Optional
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        self.diameter = 0
+
+        def trav(node):
+            if not node:
+                return 0
+
+            left_depth = trav(node.left)
+            right_depth = trav(node.right)
+            self.diameter = max(self.diameter, left_depth + right_depth)
+
+            return 1 + max(left_depth, right_depth)
+
+        trav(root)
+        return self.diameter
+
+# TC: O(n)
+# SC: O(h)
--- DSA/Trees/MaximumDepthOfABinaryTree.py
+++ DSA/Trees/MaximumDepthOfABinaryTree.py
@@ -1,9 +1,10 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right    
+
 class Solution(object):
     def maxDepth(self, root):
         if not root: 
@@ -11,4 +12,17 @@ class Solution(object):
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 # Beats 92.59% python submissions in runtime
-# Beats 73.85% python submissions in memory usage  
+# Beats 73.85% python submissions in memory usage
+
+from typing import Optional
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def trav(node):
+            if not node:
+                return 0
+            return 1 + max(trav(node.left), trav(node.right))
+        return trav(root)
+
+# TC: O(n)
+# SC: O(h)
