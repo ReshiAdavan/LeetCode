bc88c54849da764fc52fa703c4092f431e2ef491
ReshiAdavan <reshiadavan27@gmail.com>
2024-12-09 22:32:32 -0500

    LC-60: Interview Practice

--- DSA/Binary Search/SearchA2DMatrix.py
+++ DSA/Binary Search/SearchA2DMatrix.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         i = 0
         while i < len(matrix):
             l, r = 0, len(matrix[0]) - 1
+            if matrix[i][r] < target:
+                i += 1
+                continue
             while l <= r:
                 mid = (l + r) // 2
                 if matrix[i][mid] == target:
