4249c917ccae3577aea33635e1a7c1be078c951a
ReshiAdavan <reshiadavan27@gmail.com>
2024-12-18 16:33:06 -0500

    LC-66: Formatting for design problems

--- DSA/Design/InsertDeleteRandomDuplicatesO(1).py
+++ DSA/Design/InsertDeleteRandomDuplicatesO(1).py
@@ -29,8 +29,5 @@ class RandomizedCollection:
         return random.choice(self.data)
 
 
-# Your RandomizedCollection object will be instantiated and called as such:
-# obj = RandomizedCollection()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+# TC: O(1)
+# SC: O(n)
